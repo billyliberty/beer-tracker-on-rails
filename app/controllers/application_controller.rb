3e0583e470ae04bed236236dacdad7b0e5f8577b
billyliberty <joshua.crum@gmail.com>
2019-10-22 17:45:10 -0500

    Set helper method so they can be accessed throughout app.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,7 @@
 class ApplicationController < ActionController::Base
 
+    helper_method :current_user, :require_login, :logged_in?
+
     def current_user
         if session[:user_id].present?
             user = User.find_by(:id => sessionp[:user_id])

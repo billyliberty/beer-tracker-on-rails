39710595b77c3c1d78244e0435c675fc121b5cb9
billyliberty <joshua.crum@gmail.com>
2019-10-22 17:42:37 -0500

    Added helper method for setting current user.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,9 @@
 class ApplicationController < ActionController::Base
+
+    def current_user
+        if session[:user_id].present?
+            user = User.find_by(:id => sessionp[:user_id])
+        end
+    end
+    
 end

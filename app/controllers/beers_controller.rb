9ec5ad45a0fa10897f8da40de5e1b8a4eaa7ac3b
billyliberty <joshua.crum@gmail.com>
2019-10-22 17:46:47 -0500

    Updated with intitial CRUD actions.

--- app/controllers/beers_controller.rb
+++ app/controllers/beers_controller.rb
@@ -1,5 +1,20 @@
 class BeersController < ApplicationController
 
     before_action :require_login
-    
+
+    def new
+    end
+
+    def index
+    end
+
+    def show
+    end
+
+    def create
+    end
+
+    def update
+    end
+
 end

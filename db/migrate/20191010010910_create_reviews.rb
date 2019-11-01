2d097d4f5cb9f83f19fb5e8db6db1fede9211b61
billyliberty <joshua.crum@gmail.com>
2019-11-01 13:46:07 -0500

    Added details to review database migration.

--- db/migrate/20191010010910_create_reviews.rb
+++ db/migrate/20191010010910_create_reviews.rb
@@ -1,6 +1,13 @@
 class CreateReviews < ActiveRecord::Migration[5.2]
   def change
     create_table :reviews do |t|
+      t.integer :rating
+      t.text :review
+      t.decimal :price
+      t.string :store
+      t.date :date_of_purchase
+      t.references :user, foreign_key: true
+      t.references :beer, foreign_key: true
 
       t.timestamps
     end

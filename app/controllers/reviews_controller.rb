c2a5c25d6dd686a3e5bfb3f47a8579458a9f2141
billyliberty <joshua.crum@gmail.com>
2019-11-01 13:11:09 -0500

    Added basic outline for controller actions.

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -2,7 +2,25 @@ class ReviewsController < ApplicationController
 
     before_action :require_login
 
+    def index
+    end
+
+    def new
+    end
+
+    def create
+    end
+
+    def edit
+    end
+
+    def update
+    end
+
+    def show
+    end
     
+
 
 
 end

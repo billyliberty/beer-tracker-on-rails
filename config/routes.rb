bba20573d6f9812a09e2f4447ab8cb2da1114e02
billyliberty <joshua.crum@gmail.com>
2019-11-01 13:39:04 -0500

    Updated routes.

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 Rails.application.routes.draw do
+
+  resources :beers do
+    resources :reviews
+  end
+
   resources :users
   resources :reviews
-  resources :beers
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
 end

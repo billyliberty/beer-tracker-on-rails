3df5e1b53c8abab474e01f7109ae9c50a82bc496
billyliberty <joshua.crum@gmail.com>
2019-11-01 13:37:31 -0500

    added helper method for user params.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -20,4 +20,10 @@ class UsersController < ApplicationController
         end
     end
 
+    private
+
+    def user_params
+        params.require(:user).permit(:name, :email, :password)
+    end
+
 end

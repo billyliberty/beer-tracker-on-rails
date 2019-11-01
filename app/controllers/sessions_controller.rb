1103b2b272cc27353b931d596c9546004c9cf7bc
billyliberty <joshua.crum@gmail.com>
2019-11-01 13:34:06 -0500

    Added home action.

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -9,4 +9,9 @@ class SessionsController < ApplicationController
             redirect_to '/login'
         end
     end
+
+    def destroy
+        session.clear
+        redirect_to root_path
+    end
 end

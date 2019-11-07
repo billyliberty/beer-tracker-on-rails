f428940160fa8cad600015f8b415dbffcfe9a989
billyliberty <joshua.crum@gmail.com>
2019-11-07 14:45:50 -0600

    Corrected syntax issue.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,7 +5,7 @@ class ApplicationController < ActionController::Base
 
     def current_user
         if session[:user_id].present?
-            user = User.find_by(:id => sessionp[:user_id])
+            user = User.find_by(:id => session[:user_id])
         end
     end
 

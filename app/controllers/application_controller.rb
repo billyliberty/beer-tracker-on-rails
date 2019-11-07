a9fad45214e0f6da5b3e865d97aacc27144c30f4
billyliberty <joshua.crum@gmail.com>
2019-11-07 14:06:16 -0600

    Revised application controller.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,7 @@
 class ApplicationController < ActionController::Base
 
     helper_method :current_user, :require_login, :logged_in?
+    before_action :current_user, :require_login
 
     def current_user
         if session[:user_id].present?

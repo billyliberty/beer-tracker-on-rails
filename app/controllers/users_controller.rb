d4ae53ec104fa99b73e19c5ef479601f0285abb1
billyliberty <joshua.crum@gmail.com>
2019-11-07 14:04:26 -0600

    Revised user show page.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -13,7 +13,7 @@ class UsersController < ApplicationController
     def create
         user = User.create(user_params)
         if user.valid?
-            sessions[:user_id] = user.id 
+            session[:user_id] = user.id 
             redirect_to user_path(user)
         else
             render :new

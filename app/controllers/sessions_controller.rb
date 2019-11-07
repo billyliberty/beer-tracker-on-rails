1d31b8037642d4e90034b6764a8ee4d1e8ae0b12
billyliberty <joshua.crum@gmail.com>
2019-11-07 14:09:17 -0600

    Revised sessions controller.

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,5 +1,11 @@
 class SessionsController < ApplicationController
 
+    skip_before_action :require_login
+
+    def new
+        @user = User.new
+    end
+
     def create
         user = User.find_by(email: params[:user][:email])
         if user && user.authenticate(params[:user][:password])

9c3c32f30517402bd7c4c5c8e9743853f0fa3e32
billyliberty <joshua.crum@gmail.com>
2019-11-07 14:50:05 -0600

    Revised current_user method.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,10 +4,9 @@ class ApplicationController < ActionController::Base
     before_action :current_user, :require_login
 
     def current_user
-        if session[:user_id].present?
-            user = User.find_by(:id => session[:user_id])
-        end
+        @current_user ||= User.find_by(:id => session[:user_id]) if session[:user_id].present?
     end
+    
 
     def require_login
         unless current_user

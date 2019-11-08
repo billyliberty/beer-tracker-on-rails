bc2e3a65066dfd4e8c7e638518ca8d5837cb3188
billyliberty <joshua.crum@gmail.com>
2019-11-08 10:46:13 -0600

    Removed flash message for bootstrap.

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -17,8 +17,7 @@ class ReviewsController < ApplicationController
     def create
         @review = Review.new(review_params)
         if @review.save
-            redirect_to beer_review_path(@review.beer, @review), flash[:message] = "Review saved!"
-        else
+            redirect_to beer_review_path(@review.beer, @review)
             render :new
         end
     end

2986e247d496ac7d14418a580f7fb6cc9cc4b21e
billyliberty <joshua.crum@gmail.com>
2019-11-08 15:02:29 -0600

    Revised sessions controller actions and corrected references to instance variables.

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -10,12 +10,12 @@ class SessionsController < ApplicationController
         if authentication_hash = request.env["omniauth.auth"]
             @user = User.find_or_create_by_omniauth(authentication_hash)
             sessions[:user_id] = @user.id
-            redirect_to user_path(user)
+            redirect_to @user
         else
             @user = User.find_by(email: params[:user][:email])
-                if @user && user.authenticate(params[:user][:password])
-                session[:user_id] = user.id 
-                redirect_to user_path(user)
+                if @user && @user.authenticate(params[:user][:password])
+                session[:user_id] = @user.id 
+                redirect_to @user
             else
                 redirect_to '/signin'
             end

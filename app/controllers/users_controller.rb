09047994843f825a94bf24535d0fd59acfd6d0f6
billyliberty <joshua.crum@gmail.com>
2019-11-07 19:12:23 -0600

    Updated users controller actions.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -10,10 +10,10 @@ class UsersController < ApplicationController
     end
 
     def create
-        @user = User.create(user_params)
-        if @user.valid?
+        @user = User.new(user_params)
+        if @user.save
             session[:user_id] = @user.id 
-            redirect_to user_path(@user)
+            redirect_to @user
         else
             render :new
         end
